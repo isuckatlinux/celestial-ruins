a267a7d506ad94b4a9a4d5b12870b000d07f793b
Yohan Diaz <yohan.diaz5632@gmail.com>
2023-12-09 20:33:41 +0100

    adding dev env

---
+++ dev/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Example startup script
+echo "Setting up your C development environment..."
+
+# Add any additional setup or initialization commands here
+
+echo "Environment setup complete. Ready to develop in C."
