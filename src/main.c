5a85b8a04fcddfe2a13442aae2fc0868c46caa6a
Yohan Diaz <yohan.diaz5632@gmail.com>
2023-12-09 04:46:12 +0100

    Posible initial includes

---
+++ src/main.c
@@ -0,0 +1,18 @@
+// Native library includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Custom library includes
+#include "include/enemy.h"
+#include "include/game.h"
+#include "include/item.h"
+#include "include/map.h"
+#include "include/player.h"
+#include "include/ui.h"
+
+// Variables
+bool game_running = true;
